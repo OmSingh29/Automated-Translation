b79563780b5e6f41f40f02bcd28491affc4995e9
OmSingh29 <123638320+OmSingh29@users.noreply.github.com>
2024-09-09 02:34:42 +0530

    Update Translate.py

--- Translate.py
+++ Translate.py
@@ -23,7 +23,7 @@ options.add_argument("--disable-notifications")
 options.add_argument("--disable-extensions")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 options.add_argument("--incognito") 
-#options.add_argument("--headless") 
+options.add_argument("--headless") 
 
 driver = webdriver.Chrome(options=options)
 
